825e751dab9a94302bdff078712144e91c1e0e46
Isaac Wengler <iwengler@salesforce.com>
2023-10-30 00:39:03 -0500

    lsp

--- lua/isaac/lazy.lua
+++ lua/isaac/lazy.lua
@@ -21,5 +21,10 @@ require("lazy").setup({
 	{
 		'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'
 	},
-	"mbbill/undotree"
+	"mbbill/undotree",
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
 })
