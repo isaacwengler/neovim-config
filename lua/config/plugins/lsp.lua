92df13294a9050e94d2f01aa09d6147f7b3c9f3e
Isaac Wengler <iwengler3@gmail.com>
2024-03-17 23:11:37 -0500

    lua vim lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -90,9 +90,12 @@ return {
                         require('lspconfig').lua_ls.setup {
                             settings = {
                                 Lua = {
-                                    diagnostics = {
-                                        globals = { 'vim' }
-                                    }
+                                    runtime = { version = 'LuaJIT' },
+                                    workspace = {
+                                        library = vim.api.nvim_get_runtime_file('', true),
+                                        maxPreload = 10000,
+                                        preloadFileSize = 10000,
+                                    },
                                 }
                             }
                         }
