cc8d205a7eaa4c3531c42bebde600f14f7cfadc2
Isaac Wengler <iwengler3@gmail.com>
2026-01-05 16:06:56 -0500

    js tweaks

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -23,7 +23,9 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", function()
+  require("conform").format({ lsp_fallback = true })
+end)
 
 -- switch tmux session
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-session<CR>")
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -5,6 +5,15 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
 
 vim.opt.smartindent = true
 
